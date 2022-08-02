7fd1f0e040078ed84c73fe61749094e4a22c7596
JHONMENDEZ2006 <110487167+JHONMENDEZ2006@users.noreply.github.com>
2022-08-02 13:22:51 -0500

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -63,7 +63,7 @@ text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length
     
 
 let str = `
-*ミ💖 𝙷𝙾𝙻𝙰 ✨${name}✨, 𝙰𝚀𝚄𝙸 𝙴𝚂𝚃𝙰 𝙴𝙻 𝙼𝙴𝙽𝚄 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙾 𝙳𝙴 𝚃𝙷𝙴 𝙼𝚈𝚂𝚃𝙸𝙲 - 𝙱𝙾𝚃 💖彡*
+*ミ💖 𝙷𝙾𝙻𝙰 ✨${name}✨, 𝙰𝚀𝚄𝙸 𝙴𝚂𝚃𝙰 𝙴𝙻 𝙼𝙴𝙽𝚄 𝙲𝙾𝙼𝙿𝙻𝙴𝚃𝙾 𝙳𝙴 тнe в♡т_мZ2006 - 𝙱𝙾𝚃 💖彡*
 
 *📅 𝙵𝙴𝙲𝙷𝙰: ${week}, ${date}*
 *📈 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
@@ -71,50 +71,50 @@ let str = `
 
 *<𝕀ℕ𝔽𝕆ℝ𝕄𝔸ℂ𝕀𝕆ℕ 𝔻𝔼𝕃 𝔹𝕆𝕋/>*
 
-° ඬ ⃟ 💟 _${usedPrefix}grupos_
-° ඬ ⃟ 💟 _${usedPrefix}estado_
-° ඬ ⃟ 💟 _${usedPrefix}infobot_
-° ඬ ⃟ 💟 _${usedPrefix}donar_
-° ඬ ⃟ 💟 _${usedPrefix}grouplist_
-° ඬ ⃟ 💟 _${usedPrefix}owner_
-° ඬ ⃟ 💟 _${usedPrefix}script_
-° ඬ ⃟ 💟 _Bot_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
+° ඬ ⃟ 🖤 _${usedPrefix}grupos_
+° ඬ ⃟ 🖤 _${usedPrefix}estado_
+° ඬ ⃟ 🖤 _${usedPrefix}infobot_
+° ඬ ⃟ 🖤 _${usedPrefix}donar_
+° ඬ ⃟ 🖤 _${usedPrefix}grouplist_
+° ඬ ⃟ 🖤 _${usedPrefix}owner_
+° ඬ ⃟ 🖤 _${usedPrefix}script_
+° ඬ ⃟ 💟🖤 _Bot_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
 
 *<𝕌ℕ𝔼 𝕌ℕ 𝔹𝕆𝕋 𝔸 𝕋𝕌 𝔾ℝ𝕌ℙ𝕆/>*
 
-° ඬ⃟👽 _${usedPrefix}join *<enlace / link / url>*_
+° ඬ⃟👹 _${usedPrefix}join *<enlace / link / url>*_
 
 *<𝕁𝕌𝔼𝔾𝕆𝕊/>*
 
-° ඬ⃟🎖️ _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
-° ඬ⃟🎖️ _${usedPrefix}ppt *<papel / tijera /piedra>*_
-° ඬ⃟🎖️ _${usedPrefix}prostituto *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}prostituta *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}gay2 *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}lesbiana *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}pajero *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}pajera *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}puto *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}puta *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}manco *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}manca *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}rata *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}love *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}doxear *<nombre / @tag>*_
-° ඬ⃟🎖️ _${usedPrefix}pregunta *<texto>*_
-° ඬ⃟🎖️ _${usedPrefix}suitpvp *<@tag>*_
-° ඬ⃟🎖️ _${usedPrefix}slot *<apuesta>*_
-° ඬ⃟🎖️ _${usedPrefix}ttt *<nombre sala>*_
-° ඬ⃟🎖️ _${usedPrefix}delttt_
-° ඬ⃟🎖️ _${usedPrefix}simi *<texto>*_
-° ඬ⃟🎖️ _${usedPrefix}top *<texto>*_
-° ඬ⃟🎖️ _${usedPrefix}topgays_
-° ඬ⃟🎖️ _${usedPrefix}topotakus_
-° ඬ⃟🎖️ _${usedPrefix}formarpareja_
-° ඬ⃟🎖️ _${usedPrefix}verdad_
-° ඬ⃟🎖️ _${usedPrefix}reto_
-° ඬ⃟🎖️ _${usedPrefix}cancion_
-° ඬ⃟🎖️ _${usedPrefix}pista_
+° ඬ⃟💥 _${usedPrefix}mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
+° ඬ⃟💥 _${usedPrefix}ppt *<papel / tijera /piedra>*_
+° ඬ⃟💥 _${usedPrefix}prostituto *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}prostituta *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}gay2 *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}lesbiana *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}pajero *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}pajera *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}puto *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}puta *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}manco *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}manca *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}rata *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}love *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}doxear *<nombre / @tag>*_
+° ඬ⃟💥 _${usedPrefix}pregunta *<texto>*_
+° ඬ⃟💥 _${usedPrefix}suitpvp *<@tag>*_
+° ඬ⃟💥 _${usedPrefix}slot *<apuesta>*_
+° ඬ⃟💥 _${usedPrefix}ttt *<nombre sala>*_
+° ඬ⃟💥 _${usedPrefix}delttt_
+° ඬ⃟💥 _${usedPrefix}simi *<texto>*_
+° ඬ⃟💥 _${usedPrefix}top *<texto>*_
+° ඬ⃟💥 _${usedPrefix}topgays_
+° ඬ⃟💥 _${usedPrefix}topotakus_
+° ඬ⃟💥 _${usedPrefix}formarpareja_
+° ඬ⃟💥 _${usedPrefix}verdad_
+° ඬ⃟💥 _${usedPrefix}reto_
+° ඬ⃟💥 _${usedPrefix}cancion_
+° ඬ⃟💥 _${usedPrefix}pista_
 
 *<𝔸ℂ𝕋𝕀𝕍𝔸ℝ 𝕆 𝔻𝔼𝕊𝔸ℂ𝕋𝕀𝕍𝔸ℝ/>*
 
@@ -491,9 +491,9 @@ _(𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)_
 ° ඬ⃟👑 _${usedPrefix}listprem_
 `.trim()
 conn.sendHydrated2(m.chat, str, wm, pp, 'https://www.paypal.me/TheShadowBrokers133', '𝙿𝙰𝚈𝙿𝙰𝙻', 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', [
-['📮 𝙳𝙾𝙽𝙰𝚁 📮', '/donasi'],
-['🌹 𝙾𝚆𝙽𝙴𝚁 🌹', '/owner'],
-['🐾 𝙸𝙽𝙵𝙾𝙱𝙾𝚃 🐾', '/infobot']
+['🖤 𝙳𝙾𝙽𝙰𝚁 🖤', '/donasi'],
+['🇨🇴 𝙾𝚆𝙽𝙴𝚁 🇨🇴', '/owner'],
+['👌 𝙸𝙽𝙵𝙾𝙱𝙾𝚃 🥵', '/infobot']
 ], m,)
 //await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, {
 //type: 'audioMessage', 
