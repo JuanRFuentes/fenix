53c5e9c7ba3c660d787c9210d642e4319638e1d5
JHONMENDEZ2006 <110487167+JHONMENDEZ2006@users.noreply.github.com>
2022-08-09 22:45:55 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,34 +1,22 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let picture = './Menu2.jpg'
-let name = await conn.getName(m.sender)
 let _uptime = process.uptime() * 1000
 let _muptime
 if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 1000) }) * 1000}
 let uptime = clockString(_uptime)
-let estado =`
-╭─[ *тн𝒆 в𝓸т_м𝔃𝟐𝟎𝟎𝟔 - 𝐁𝐨𝐭* ]
-│ *➤ 𝙷𝙾𝙻𝙰 ${name}*
-│
-│ *ミ🚨 𝙴𝚂𝚃𝙰𝙳𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃 🚨彡*
-│ *=> 𝙱𝙾𝚃 𝙰𝙲𝚃𝙸𝚅𝙾 ✅*
-│ *=> 𝙱𝙾𝚃 𝙳𝙴 𝚄𝚂𝙾 𝙿𝚄𝙱𝙻𝙸𝙲𝙾 ✅*
-│ *=> 𝚃𝙸𝙴𝙼𝙿𝙾 𝙰𝙲𝚃𝙸𝚅𝙾: ${uptime}*
-╰───────────────
-`.trim()
-
-conn.sendHydrated(m.chat, estado, wm, picture, 'https://github.com/JHONMENDEZ2006/Bot_mz2006_v2', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
-], m)}
-
-handler.help = ['estado']
-handler.tags = ['main']
+let texto = `${uptime}`
+let fakemsg = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: "17608914335-1625305606@g.us" } : {}) }, message: { "extendedTextMessage": { "text":`Activo por ${texto}`, "title": 'Kitzia-MD', 'jpegThumbnail': false}}}
+conn.reply(m.chat, `hola`, fakemsg)}
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
 export default handler
 
 function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+  let ye = isNaN(ms) ? '--' : Math.floor(ms / 31104000000) % 10
+  let mo = isNaN(ms) ? '--' : Math.floor(ms / 2592000000) % 12
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000) % 30
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [' *Días* •', d, ' *Horas* •', h, ' *Minutos* •', m, ' *Segundos* •', s].map(v => v.toString().padStart(2, 0)).join('')
+}
